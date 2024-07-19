ff46b3222c78c5a48f323a5f3461001abc837cb2
habtesellasie <havecodesoft@gmail.com>
2024-07-19 06:52:17 +0300

    Navbar animation removed

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,42 +1,12 @@
-'use client';
-import { motion } from 'framer-motion';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-
-const projectsContainerVariants = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-const projectsChildVariants = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: {
-      ease: 'ease',
-      duration: 0.5,
-    },
-  },
-};
-
 const Navbar = () => {
   const pathname = usePathname();
 
   return (
     <nav className='mt-4 mx-4 mb-8'>
-      <motion.div
-        className='flex gap-4 items-center mt-4'
-        variants={projectsContainerVariants}
-        initial='hidden'
-        whileInView='visible'
-        viewport={{ once: true }}
-      >
-        <motion.div variants={projectsChildVariants}>
+      <div className='flex gap-4 items-center mt-4'>
+        <div>
           <Link
             href='/'
             className={`flex items-center justify-start gap-2 underline-offset-4 hover:underline ${
@@ -46,8 +16,8 @@ const Navbar = () => {
             {/* <HiHome size={20} /> */}
             Home
           </Link>
-        </motion.div>
-        <motion.div variants={projectsChildVariants}>
+        </div>
+        <div>
           <Link
             href='/about'
             className={`flex items-center justify-start gap-2 underline-offset-4 hover:underline ${
@@ -57,8 +27,8 @@ const Navbar = () => {
             {/* <HiInformationCircle size={20} /> */}
             About
           </Link>
-        </motion.div>
-      </motion.div>
+        </div>
+      </div>
     </nav>
   );
 };
